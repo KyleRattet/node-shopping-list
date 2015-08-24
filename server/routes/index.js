3e6bea33160f381661e47c8deb0b81ade839673b
KyleRattet <krattet@gmail.com>
2015-08-24 16:25:56 -0600

    built out constructor class, additem method, and created a few instances

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,7 +7,21 @@ function ItemLibrary () {
   this.id = 0;
 }
 
-//Item Method
+//Item Methods
+ItemLibrary.prototype.addItem = function (name) {
+  var newItem = {
+    name: name,
+    id: this.id
+  };
+  this.items.push(newItem);
+  this.id += 1;
+};
+
+//test instances
+var storage = new ItemLibrary ();
+storage.addItem('apples');
+storage.addItem('bananas');
+storage.addItem('oranges');
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
