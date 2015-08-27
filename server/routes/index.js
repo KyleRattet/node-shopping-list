989850105b5b40c459bb7bf4fd4ca9bad3ae63c5
KyleRattet <krattet@gmail.com>
2015-08-27 07:46:49 -0600

    refactored put route

--- server/routes/index.js
+++ server/routes/index.js
@@ -29,66 +29,82 @@ router.get('/items', function(req, res, next) {
   res.json(storage.items);
 });
 
-//POST single item
+//POST route refactored
 router.post('/items', function(req, res) {
-  //filter to see if item already exists
-  var listItem = storage.items.filter(function (item) {
-    return item.name.toLowerCase() === req.body.name.toLowerCase();
-
-  });
-  // console.log(listItem);
-
-  if (listItem.length > 0) {
-    res.json({
-      message: "Item already in the shopping list."
-    });
-  } else {
-    var newItem = storage.addItem(req.body.name);
-    res.json({
-      message: "Success",
-      itemList: storage.items
-    });
-
-  }
+  var response = logic.handlePost(req.body.name, storage.items);
+  res.json(response);
 
 });
 
-// //put single item
+//POST  item - original
+// router.post('/items', function(req, res) {
+//   //filter to see if item already exists
+//   var listItem = storage.items.filter(function (item) {
+//     return item.name.toLowerCase() === req.body.name.toLowerCase();
+
+//   });
+//   // console.log(listItem);
+
+//   if (listItem.length > 0) {
+//     res.json({
+//       message: "Item already in the shopping list."
+//     });
+//   } else {
+//     var newItem = storage.addItem(req.body.name);
+//     res.json({
+//       message: "Success",
+//       itemList: storage.items
+//     });
+
+//   }
+
+// });
+
+//refactored put
 router.put('/item/:id', function(req, res, next) {
-
-  // //validating to see if it exists
-  var listItem = storage.items.filter(function (item) {
-    return item.id === parseInt(req.params.id);
-  });
-
-
-  //if item with that id exists, do the following
-  if (listItem.length>0){
-
-  //looping through storage items to find object with that id, and then changing it to whatever req.body.name input in httpie is
-    for (var i = 0; i < storage.items.length; i++) {
-       if (storage.items[i].id === parseInt(req.params.id)) {
-          for (var key in req.body) {
-            if (key === 'name') {
-              storage.items[i].name = req.body.name;
-            } else if (key === 'id') {
-              storage.items[i].id = req.body.id;
-            }
-        }
-      }
-    }
-    //updated on the browser
-    res.json(storage.items);
-  } else {
-    var newItem = storage.addItem(req.body.name);
-    res.json({
-      message: "Success",
-      itemList: storage.items
-    });
-
-  }
+  var response = logic.handlePut(req.params.id, req.body, req.body.name, req.body.id, storage.items);
+  res.json(response);
 
 });
+
+// //put single item version 1
+// router.put('/item/:id', function(req, res, next) {
+
+//   // //validating to see if it exists
+//   var listItem = storage.items.filter(function (item) {
+//     return item.id === parseInt(req.params.id);
+//   });
+
+//   console.log(req.body, "req body");
+//   console.log(req.body.name, "req body name");
+//   console.log(req.body.id, "req body id");
+//   //if item with that id exists, do the following
+//   if (listItem.length>0){
+
+//   //looping through storage items to find object with that id, and then changing it to whatever req.body.name input in httpie is
+//     for (var i = 0; i < storage.items.length; i++) {
+//        if (storage.items[i].id === parseInt(req.params.id)) {
+//           for (var key in req.body) {
+//             if (key === 'name') {
+//               storage.items[i].name = req.body.name;
+//             } else if (key === 'id') {
+//               storage.items[i].id = req.body.id;
+//             }
+//         }
+//       }
+//     }
+//     //updated on the browser
+//     res.json(storage.items);
+//   } else {
+//     var newItem = storage.addItem(req.body.name);
+//     res.json({
+//       message: "Success",
+//       itemList: storage.items
+//     });
+
+//   }
+
+// });
 
 
 //refactored delete route
--- server/utilities/logic.js
+++ server/utilities/logic.js
@@ -1,3 +1,64 @@
+//handle post
+function handlePost (bodyName, storageArray) {
+    //filter to see if item already exists
+  var listItem = storageArray.filter(function (item) {
+    return item.name.toLowerCase() === bodyName.toLowerCase();
+
+  });
+  console.log(listItem, "list item");
+
+  console.log(bodyName, "body name var");
+
+  if (listItem.length > 0) {
+     console.log(storageArray, "storage array");
+    return({
+      message: "Item already in the shopping list."
+    });
+  } else {
+     var newItem = storage.addItem(bodyName);
+    return ({
+      message: "Success",
+      itemList: storageArray
+    });
+  }
+}
+
+
+///handle put
+function handlePut (itemID, body, bodyName, bodyID, storageArray) {
+    // //validating to see if it exists
+  var listItem = storageArray.filter(function (item) {
+    return item.id === parseInt(itemID);
+  });
+
+
+  //if item with that id exists, do the following
+  if (listItem.length>0){
+
+  //looping through storage items to find object with that id, and then changing it to whatever req.body.name input in httpie is
+    for (var i = 0; i < storageArray.length; i++) {
+       if (storageArray[i].id === parseInt(itemID)) {
+          for (var key in body) {
+            if (key === 'name') {
+              storageArray[i].name = bodyName;
+            } else if (key === 'id') {
+              storageArray[i].id = bodyID;
+            }
+        }
+      }
+    }
+    //updated on the browser
+    return (storageArray);
+  } else {
+    var newItem = storage.addItem(bodyName);
+    return ({
+      message: "Success",
+      itemList: storageArray
+    });
+
+  }
+}
+
 //handle delete
 function handleDelete(itemID, storageArray) {
   var listItem = storageArray.filter(function(item){
@@ -23,5 +84,7 @@ function handleDelete(itemID, storageArray) {
 //export function
 
 module.exports = {
+  handlePost: handlePost,
   handleDelete: handleDelete,
+  handlePut: handlePut,
 };
